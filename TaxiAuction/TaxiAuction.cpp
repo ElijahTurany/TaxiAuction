c9c7fc7bef66882cae3bb02a1400312145077e78
Liam1017 <elijahturany1415@gmail.com>
2023-05-02 23:25:33 -0500

    2023/05/02 23:25

--- TaxiAuction/TaxiAuction.cpp
+++ TaxiAuction/TaxiAuction.cpp
@@ -4,24 +4,35 @@ using namespace std;
 
 int main() {
     int input = 0;
+    maxHeap h;
     while (input != 6) {
-        maxHeap h;
         cout << "Enter a number\n1 to add a new bid\n2 to get the highest bid\n3 to remove the highest bid\n4 to remove a bid by name\n5 to print all bids\n6 to quit\n";
         cin >> input;
         if (input == 1) {
-
+            string name;
+            int bid;
+            cout << "Enter name: ";
+            cin >> name;
+            cout << "Enter bid: ";
+            cin >> bid;
+            h.enqueue(bid, name);
         }
         if (input == 2) {
-
+            pair<int, string> top = h.peek();
+            cout << "Name: " << top.second << " Bid: " << top.first << endl;
         }
         if (input == 3) {
-
+            pair<int, string> top = h.dequeue();
+            cout << "Name: " << top.second << " Bid: " << top.first << " Removed" << endl;
         }
         if (input == 4) {
-
+            string name;
+            cout << "Enter name: ";
+            cin >> name;
+            h.removeAny(name);
         }
         if (input == 5) {
-
+            h.print();
         }
     }   
 }
